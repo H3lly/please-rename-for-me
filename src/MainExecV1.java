e894d887426bda1ce49af83ded537b2b1e9fe9df
H3lly <h3lly.h0lly@gmail.com>
2016-08-31 20:11:43 +0200

    Factorisation

--- src/MainExecV1.java
+++ src/MainExecV1.java
@@ -5,25 +5,22 @@ public class MainExecV1 {
   }
 
   public static String[] getFileListV1() {
-    String[] fileList = { "Sense8 S1E1", "S01E02", "1X3", "Saison 1 Episode 4", "Saison01.Episode05 " };
+    String[] fileList = { "Amercian Horror Story S1E1", "american horror story S01E02", "american horror story 1X3",
+        "AHS Saison 1 Episode 4", "ash Saison01.Episode05 " };
     return fileList;
   }
-
 
   public static String[] rename(String[] fileList, /* String nameFormat, */ String name) {
     for (int i = 0; i < fileList.length; ++i) {
       String[] tab = testRegex.match(".*S(\\d+)E(\\d+).*", fileList[i]);
       if (tab[0] != "None") {
-        String tmp = tab[0];
         String season = tab[1];
         if (season.length() == 1)
           season = "0" + season;
         String episode = tab[2];
         if (episode.length() == 1)
           episode = "0" + episode;
-        fileList[i] = name + " - " + "S" + season + " E" + episode;
-        System.out.println(
-            "Original :" + tmp + " | Episode " + episode + " | Season " + season + " | Final : " + fileList[i]);
+        fileList[i] = name + " S" + season + "E" + episode;
       } else {
         tab = testRegex.match(".*(\\d+).(\\d+).*", fileList[i]);
         if (tab[0] != "None") {
@@ -33,7 +30,7 @@ public class MainExecV1 {
           String episode = tab[2];
           if (episode.length() == 1)
             episode = "0" + episode;
-          fileList[i] = name + " - " + "S" + season + " E" + episode;
+          fileList[i] = name + " S" + season + "E" + episode;
         } else {
           tab = testRegex.match(".*.aison ?(\\d+).*.pisode ?(\\d+) ?.*", fileList[i]);
           if (tab[0] != "None") {
@@ -43,7 +40,7 @@ public class MainExecV1 {
             String episode = tab[2];
             if (episode.length() == 1)
               episode = "0" + episode;
-            fileList[i] = name + " - " + "S" + season + " E" + episode;
+            fileList[i] = name + " S" + season + "E" + episode;
           }
         }
       }
@@ -51,10 +48,15 @@ public class MainExecV1 {
     return fileList;
   }
 
+  public static String getName() {
+    String tmp = "American Horror Story";
+    return tmp;
+  }
+
   public static void main(String[] args) {
     String[] fileList = getFileListV1();
-    String[] renamed = rename(fileList, "American Horror Story");
-    System.out
-        .println("\n" + renamed[0] + "\n" + renamed[1] + "\n" + renamed[2] + "\n" + renamed[3] + "\n" + renamed[4]);
+    String name = getName();
+    String[] renamed = rename(fileList, name);
+    System.out.println(renamed[0] + "\n" + renamed[1] + "\n" + renamed[2] + "\n" + renamed[3] + "\n" + renamed[4]);
   }
 }
